874263d533d4abb044833f243d0427c1bac22302
JulioAlberto89 <jamv19.06.89@gmail.com>
2024-02-20 21:38:49 +0100

    Mapa completado

---
+++ assets/Wood_Tower_Blue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Wood_Tower_Blue" tilewidth="32" tileheight="32" tilecount="192" columns="32">
+ <image source="Wood_Tower_Blue.png" width="1024" height="192"/>
+</tileset>
